01b8abea4e2ff1dd238fa461b97859a6da6f48dd
DimitarIvVelikov <dimitar.ivvelikov@gmail.com>
2023-05-27 02:00:46 +0300

    Fixed bug on gladiator exercis

--- Data Types and Variables/GladiatorExpenses.js
+++ Data Types and Variables/GladiatorExpenses.js
@@ -4,15 +4,22 @@ function gladiatorExpenses(lostFights,helmP,swordP,shieldP,armorP) {
     let repairs = [0,0,0,0];
  for (let i = 1; i <= lostFights; i++) {
     totalLoses = i;
-    if(shieldBreakCounter % 2 === 0){
-        repairs[3]++;
+    
+
+    if(totalLoses % 6 === 0){ 
+        repairs[0]++; 
+        repairs[1]++; 
+        repairs[2]++; 
+        shieldBreakCounter++; 
+        if(shieldBreakCounter % 2 === 0 && shieldBreakCounter !== 0){
+            repairs[3]++;
+        }
     }
-    if(totalLoses % 6 === 0){ repairs[0]++; repairs[1]++; repairs[2]++; }
     else if (totalLoses % 3 === 0) { repairs[1]++; }
     else if (totalLoses % 2 === 0 ) { repairs[0]++; }
  }
  let totalPrice= repairs[0]*helmP + repairs[1]*swordP + repairs[2]*shieldP + repairs[3]*armorP;
 
-    console.log(`Gladiator expenses: ${totalPrice} aureus`);
+    console.log(`Gladiator expenses: ${totalPrice.toFixed(2)} aureus`);
 }
-gladiatorExpenses(7,2,3,4,5);
+gladiatorExpenses(23,12.50,21.50,40,200);
