425084d5707c2f563cfdc86149fdc1b4f57ae735
DimitarIvVelikov <dimitar.ivvelikov@gmail.com>
2023-05-27 23:11:47 +0300

    Added one exercise

--- Data Types and Variables/SpiceMustFlow.js
+++ Data Types and Variables/SpiceMustFlow.js
@@ -1,25 +1,26 @@
 function spiceMustFlow(startingYield) {
     let currentYield = startingYield;
+    let CrewConsump = 26;
     let totalYield = 0;
     let days = 0;
     while(currentYield > 99){
         days++;
-        totalYield+=currentYield;
-        if (totalYield <= 26) {
+        totalYield+=currentYield;// totalYield = totalYield + currentYield;
+        if (totalYield <= CrewConsump) {
             totalYield = 0;
         }else{
-            totalYield-=26;
+            totalYield-=CrewConsump;
         }
-        currentYield-=10;
+        currentYield-=10;// currentYield = currentYield- 10;
         if (currentYield < 100) {
-            if (totalYield <= 26) {
+            if (totalYield <= CrewConsump) {
                 totalYield = 0;
             }else{
-                totalYield-=26;
+                totalYield-=CrewConsump;
             }
         }
     }
     console.log(days);
     console.log(totalYield);
 }
-spiceMustFlow(450);
+spiceMustFlow(100);
